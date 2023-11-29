5e847780449e16ce1d0ca7e9cba6b04e04db1ed5
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-11-29 14:57:03 +0900

    rename

--- etude/takeArgsFromCommandLine.ts
+++ etude/takeArgsFromCommandLine.ts
@@ -4,4 +4,5 @@ import { parse } from "https://deno.land/std/flags/mod.ts";
 const args = parse(Deno.args);
 const project = args.project ?? "villagepump";
 const outfile = args.outfile ?? "./data,json";
-console.log(project, outfile);
+const a_b = args["a-b"] ?? "a";
+console.log(project, outfile, a_b);
