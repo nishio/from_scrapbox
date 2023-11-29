16a4876c2c73fe634108e505e23f379a8776bfee
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-11-29 16:14:15 +0900

    some stats

--- etude/stat.py
+++ etude/stat.py
@@ -12,28 +12,103 @@ def find_me():
                 print(line["userId"])
                 break
 
-num_pages = 0
-num_my_pages = 0
-num_pages_with_me = 0
+def stat1():
+    num_pages = 0
+    num_my_pages = 0
+    num_pages_with_me = 0
 
-num_lines = 0
-num_my_lines = 0
-for p in pages:
-    num_pages += 1
-    if p["user"]["id"] == nishio_user_id:
-        num_my_pages += 1
-    if any(x["id"] == nishio_user_id for x in p["collaborators"]):
-        num_pages_with_me += 1
-    for line in p["lines"]:
-        num_lines += 1
-        if line["userId"] == nishio_user_id:
-            num_my_lines += 1
+    num_lines = 0
+    num_my_lines = 0
 
-print("num_pages", num_pages, "num_my_pages", num_my_pages, "num_pages_with_me", num_pages_with_me)
-print("num_lines", num_lines, "num_my_lines", num_my_lines)
+    for p in pages:
+        num_pages += 1
+        if p["user"]["id"] == nishio_user_id:
+            num_my_pages += 1
+        if any(x["id"] == nishio_user_id for x in p["collaborators"]):
+            num_pages_with_me += 1
+        for line in p["lines"]:
+            num_lines += 1
+            if line["userId"] == nishio_user_id:
+                num_my_lines += 1
 
-"""
-Result:
-num_pages 22977 num_my_pages 1342 num_pages_with_me 1989
-num_lines 740316 num_my_lines 65458
-"""
+    print("num_pages", num_pages, "num_my_pages", num_my_pages, "num_pages_with_me", num_pages_with_me)
+    print("num_lines", num_lines, "num_my_lines", num_my_lines)
+
+    """
+    Result:
+    num_pages 22977 num_my_pages 1342 num_pages_with_me 1989
+    num_lines 740316 num_my_lines 65458
+    """
+
+def stat2():
+    from collections import defaultdict
+    ratio_stats = defaultdict(int)
+
+    num_pages = 0
+
+    for p in pages:
+        num_pages += 1
+        num_lines = 0
+        num_my_lines = 0
+        for line in p["lines"]:
+            num_lines += 1
+            if line["userId"] == nishio_user_id:
+                num_my_lines += 1
+        ratio = int(100 * num_my_lines / num_lines)
+        ratio_stats[ratio] += 1
+
+    print(ratio_stats)
+    print([ratio_stats[i] for i in range(0, 101)])
+
+    import matplotlib.pyplot as plt
+    plt.plot([ratio_stats[i] for i in range(1, 101)])
+
+def stat3():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    x = []
+    y = []
+
+    for p in pages:
+        num_lines = 0
+        num_my_lines = 0
+        for line in p["lines"]:
+            num_lines += 1
+            if line["userId"] == nishio_user_id:
+                num_my_lines += 1
+        if num_my_lines == 0:
+            continue
+        x.append(num_lines)
+        y.append(num_my_lines)
+
+    sns.jointplot(x=np.log10(x), y=np.log10(y), alpha=0.1, kind='scatter')
+    plt.show()
+    sns.jointplot(x=np.log10(x), y=np.log10(y), kind='kde')
+    plt.show()
+
+
+def stat4():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    x = []
+    y = []
+
+    for p in pages:
+        if p["title"].startswith("20"):
+            continue
+        num_lines = 0
+        num_my_lines = 0
+        for line in p["lines"]:
+            num_lines += 1
+            if line["userId"] == nishio_user_id:
+                num_my_lines += 1
+        if num_my_lines == 0:
+            continue
+        x.append(num_lines)
+        y.append(num_my_lines / num_lines)
+
+    sns.jointplot(x=np.log10(x), y=y, alpha=0.1, kind='scatter')
+    plt.show()
+    # sns.jointplot(x=np.log10(x), y=y, kind='kde')
